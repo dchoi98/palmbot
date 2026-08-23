b732bbfd1e34e86f20aede0964f54f8245b0a00a
Derrick Choi <derrickchoi98@gmail.com>
2026-08-22 22:59:11 -0400

    Add robot URDF and launch files for loading robot into RViz and Gazebo

---
+++ ros2/palmbot_description/launch/rviz_launch.py
@@ -0,0 +1,37 @@
+import os
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command, LaunchConfiguration
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    pkg_share = FindPackageShare(package='palmbot_description').find('palmbot_description')
+    default_model_path = os.path.join(pkg_share, 'urdf', 'palmbot_description.xacro')
+    default_rviz_config_path = os.path.join(pkg_share, 'rviz', 'config.rviz')
+    load_description_path = os.path.join(pkg_share, 'launch', 'load_description_launch.py')
+
+    joint_state_publisher_node = Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher',
+        parameters=[{'robot_description': Command(['xacro ', default_model_path])}]
+    )
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+        arguments=['-d', LaunchConfiguration('rvizconfig')],
+    )
+
+    return LaunchDescription([
+        DeclareLaunchArgument(name='model', default_value=default_model_path, description='Absolute path to robot model file'),
+        DeclareLaunchArgument(name='rvizconfig', default_value=default_rviz_config_path, description='Absolute path to rviz config file'),
+        IncludeLaunchDescription(PythonLaunchDescriptionSource(load_description_path)),
+        joint_state_publisher_node,
+        rviz_node
+    ])
